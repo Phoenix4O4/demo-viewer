2f0cd12e2d01d1e7da4485a245b4896d6c08bbc6
dwasint <82520990+dwasint@users.noreply.github.com>
2023-06-19 03:48:04 -0400

    Update player.ts

--- src/player/player.ts
+++ src/player/player.ts
@@ -452,7 +452,7 @@ export class DemoPlayer {
 			}
 			let root_appearance = thing.get_appearance(this, see_invisible);
 			if(!root_appearance || root_appearance.invisibility > see_invisible) continue;
-			if(root_appearance.plane == 15 && !this.show_darkness) continue;
+			if(root_appearance.plane == 10 && !this.show_darkness) continue;
 			for(let appearance of Appearance.get_appearance_parts(root_appearance)) {
 				if(!appearance.icon_state_dir) {
 					let dir = this.get_appearance_dir(appearance, buffer.atlas);
