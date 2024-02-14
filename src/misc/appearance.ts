fd4d6f94bb9af2b401141c0979f651011a291e51
dwasint <82520990+dwasint@users.noreply.github.com>
2024-02-14 03:29:17 -0500

    engrains lighting into the viewer

--- src/misc/appearance.ts
+++ src/misc/appearance.ts
@@ -239,11 +239,10 @@ export namespace Appearance {
 				overlay = {...overlay};
 			}
 		}
-		if(!(overlay.plane == 13 || overlay.plane == -197 || overlay.plane == -407 || overlay.plane == -617 || overlay.plane == 11) && (overlay.layer == -32 || overlay.layer == -31 || overlay.layer == -30 || overlay.layer == -29 || overlay.layer == -28 || overlay.layer == -31 || overlay.layer == -30 || overlay.layer == -29 || overlay.layer == -28 || overlay.layer == -27 || overlay.layer == -26 || overlay.layer == -25 || overlay.layer == -24
-			|| overlay.layer == -23 || overlay.layer == -22 || overlay.layer == -21 || overlay.layer == -20|| overlay.layer == -19 || overlay.layer == -18 || overlay.layer == -17 || overlay.layer == -16|| overlay.layer == -15 || overlay.layer == -14 || overlay.layer == -13 || overlay.layer == -12|| overlay.layer == -11 || overlay.layer == -10 || overlay.layer == -9 || overlay.layer == -8
-			|| overlay.layer == -7 || overlay.layer == -6 || overlay.layer == -5 || overlay.layer == -4|| overlay.layer == -3 || overlay.layer == -2 || overlay.layer == -1)){
-			overlay.layer *=1
-			overlay.plane = appearance.plane + 0.1
+		if(overlay.plane == 12){
+			overlay.plane = 10
+			overlay.layer = 0.02
+			overlay.blend_mode = 4
 		}
 		if(overlay.plane == 11){
 			overlay.plane = 10
@@ -285,9 +284,6 @@ export namespace Appearance {
 		if(overlay.plane < Planes.LOWEST_EVER_PLANE || overlay.plane > Planes.HIGHEST_EVER_PLANE) {
 			clone();
 			overlay.plane = resolve_plane(overlay.plane, appearance.plane);
-		}
-		if(overlay.plane == 10){
-			overlay.color_alpha = (245 << 24)
 		}
 
 		return overlay;
